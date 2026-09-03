1b38b04edfc330f3a81b4cb271784430cff43069
tarielmyrzakulov05-ux <tarielmyrzakulov05@gmail.com>
2026-09-03 16:30:49 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = 5510300316
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -18,6 +19,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Мы ответим и пришлём результат в течение нескольких часов!"
     )
 
+async def forward_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # пересылаем админу текст, фото и любые другие сообщения от клиентов
+    await context.bot.forward_message(
+        chat_id=ADMIN_ID,
+        from_chat_id=update.effective_chat.id,
+        message_id=update.message.message_id
+    )
+
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(~filters.COMMAND, forward_to_admin))
 app.run_polling()
